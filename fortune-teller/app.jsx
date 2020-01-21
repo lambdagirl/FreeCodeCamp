070789166cd4918b707b6c5b96951bddd67d4aff
Peizhi Zheng <parisa1206@gmail.com>
2020-01-20 19:28:08 -0800

    delete

--- fortune-teller/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Lucky from './lucky';
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  ReactDOM.render(<Lucky />, root);
-});
